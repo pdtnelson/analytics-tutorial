2d3c86dc022a7281e042bcc2e382454af0b283c4
pdtnelson <pdtnelson@gmail.com>
2024-09-10 02:57:27 -0600

    Update AppServiceProvider.php
    Typo...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(App::environment('production')) {
-            URL::formatScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
